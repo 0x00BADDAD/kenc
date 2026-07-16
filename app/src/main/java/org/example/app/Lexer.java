36021f5bad501167bf8136d0c34a2d8346997689
debath <debath01@debath.debath>
2026-07-16 11:08:54 +0530

    Initial commit

---
+++ app/src/main/java/org/example/app/Lexer.java
@@ -0,0 +1,11 @@
+package org.kenlang.app;
+
+
+public class Lexer {
+    public static void main(String[] args) {
+        LinkedList tokens;
+        tokens = split(getMessage());
+        String result = join(tokens);
+        System.out.println(WordUtils.capitalize(result));
+    }
+}

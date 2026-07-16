b53d35ec499092b75985bed72334211495fed2d6
0x00BADDAD <tclatharvbhagya@INLTTCL00823.corp.tatacapital.com>
2026-07-16 04:06:22 +0000

    Initial commit

---
+++ lexer/src/main/java/org/kenlang/lexer/Lexer.java
@@ -0,0 +1,19 @@
+package org.kenlang.lexer;
+
+
+public class Lexer {
+    // this class provides methods to generate DFA from a bunch of regex rules
+    private String prog;
+
+    public Lexer(String prog){
+        this.prog = prog;
+        
+    }
+
+
+
+
+
+
+
+}

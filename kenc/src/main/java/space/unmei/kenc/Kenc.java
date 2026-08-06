395431a951b5a0096c8e21509185530c18ab8200
0x00BADDAD <tclatharvbhagya@INLTTCL00823.corp.tatacapital.com>
2026-08-06 14:16:07 +0000

    cleanup - removed redundant libraries

---
+++ kenc/src/main/java/space/unmei/kenc/Kenc.java
@@ -0,0 +1,9 @@
+package space.unmei.kenc;
+
+
+
+public class Kenc {
+    public static void main(String[] args) {
+        //
+    }
+}
---
+++ kenc/src/test/java/space/unmei/kenc/KencTest.java
@@ -0,0 +1,11 @@
+package space.unmei.kenc;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class KencTest {
+    @Test void testKenc() {
+        //
+    }
+}
